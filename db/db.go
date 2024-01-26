e02bfec9a54a7ea79d4c8dd4f5d05807dfe202ca
nikitazapekin <wotblitz362@mail.ru>
2024-01-26 11:28:33 +0300

    add to database

--- db/db.go
+++ db/db.go
@@ -1,18 +1,13 @@
-package db
 
+package db
 import (
 	"database/sql"
 	"fmt"
-	//	"fmt"
-	//"os"
-	//	"time"
-	"io/ioutil"
-	"os"
-    "path/filepath"
+	//"io/ioutil"
+//	"os"
+	//"path/filepath"
 	"log"
-
 	_ "github.com/lib/pq"
-	"github.com/go-pg/migrations/v8"
 )
 
 var (
@@ -20,86 +15,121 @@ var (
 )
 
 func Connect() {
-fmt.Println("DB WORK")
+    fmt.Println("DB WORK")
+    dbConnStr := "user=Nikita password=Backend dbname=golang-database sslmode=disable"
+    db, err := sql.Open("postgres", dbConnStr)
+    if err != nil {
+        log.Fatalf("Failed to open database connection: %v", err)
+    }
+    DB = db 
+    CreateTable()
+    InsertData("John Doe", "password123")
+    InsertData("Тшллл", "password12ццацйа3")
+    err = db.Ping()
+    if err != nil {
+        log.Fatalf("Failed to ping database: %v", err)
+    }
+}
+func PingDB() error {
+    err := DB.Ping()
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+func CreateTable() {
+    // Ensure that the Connect function has been called before creating the table
+    if DB == nil {
+        log.Fatal("Database connection is not established. Call Connect function first.")
+    }
+
+    // SQL query to create the table
+    query := `
+        CREATE TABLE IF NOT EXISTS TESTTABLE (
+            name VARCHAR(255),
+            password VARCHAR(255)
+        );
+    `
+
+    _, err := DB.Exec(query)
+    if err != nil {
+        log.Fatalf("Failed to create table: %v", err)
+    }
+
+    fmt.Println("Table TESTTABLE created successfully.")
+}
+
+func InsertData(name, password string) {
+    // Ensure that the Connect function has been called before inserting data
+    if DB == nil {
+        log.Fatal("Database connection is not established. Call Connect function first.")
+    }
+
+    // SQL query to insert data into the table
+    query := "INSERT INTO TESTTABLE (name, password) VALUES ($1, $2);"
+
+    _, err := DB.Exec(query, name, password)
+    if err != nil {
+        log.Fatalf("Failed to insert data into TESTTABLE: %v", err)
+    }
+
+    fmt.Println("Data inserted into TESTTABLE successfully.")
+}
+
+
+/*
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	DB *sql.DB
+)
+
+func Connect() {
+	fmt.Println("DB WORK")
 
 	dbConnStr := "user=Nikita password=Backend dbname=golang-database sslmode=disable"
-	db, err := sql.Open("postgres", dbConnStr)
+	var err error
+	DB, err = sql.Open("postgres", dbConnStr)
 	if err != nil {
 		log.Fatalf("Failed to open database connection: %v", err)
 	}
-	defer db.Close()
 
-
-	oldVersion, newVersion, err := migrations.Run(db, "up")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Printf("Migrated from version %d to %d\n", oldVersion, newVersion)
-
-	err = db.Ping()
-	//err = CreateTables()
+	err = DB.Ping()
 	if err != nil {
 		log.Fatalf("Failed to ping database: %v", err)
 	}
-	/*_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS example_table (
-			id SERIAL PRIMARY KEY,
-			test VARCHAR
-		)
-	`)
-	if err != nil {
-		log.Fatalf("Failed to create table: %v", err)
-	}
-	_, err = db.Exec("INSERT INTO example_table (test) VALUES ($1)", "Hello, World!")
-	if err != nil {
-		log.Fatalf("Failed to insert data into table: %v", err)
-	}
-*/
 }
-/*
-func CreateTables() error {
-    // Read SQL file content
-    sqlFile, err := ioutil.ReadFile("server/migrations/users_schema")
-    if err != nil {
-        return err
-    }
-
-    // Execute SQL statements
-    _, err = DB.Exec(string(sqlFile))
-    if err != nil {
-        return err
-    }
-
-    return nil
-} */
-
 
 func CreateTables() error {
-    // Get the current working directory
-    wd, err := os.Getwd()
-    if err != nil {
-        return err
-    }
-
- 
-   // filePath := filepath.Join(wd, "C:\Users\wotbl\fullstackworklist\server\server\migrations\users_schema.sql")
-	//	"server/migrations/users_schema.sql")
+	// Get the current working directory
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
 	filePath := filepath.Join(wd, "/migrations/users_schema.sql")
-    // Read SQL file content C:/Users/wotbl/fullstackworklist/
-    sqlFile, err := ioutil.ReadFile(filePath)
-    if err != nil {
-        return err
-    }
+	sqlFile, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
 
-    // Execute SQL statements
-    _, err = DB.Exec(string(sqlFile))
-    if err != nil {
-        return err
-    }
+	_, err = DB.Exec(string(sqlFile))
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 func PingDB() error {
@@ -108,4 +138,29 @@ func PingDB() error {
 		return err
 	}
 	return nil
-}  
+}
+
+func CreateUsersTable() error {
+	// SQL statement to create the table
+	createTableSQL := `
+	CREATE TABLE IF NOT EXISTS createdAtGolang (
+		id SERIAL PRIMARY KEY,
+		username VARCHAR(50) NOT NULL,
+		password VARCHAR(50) NOT NULL
+	);
+	`
+
+	_, err := DB.Exec(createTableSQL)
+	if err != nil {
+		return err
+	}
+
+	// Insert a sample user
+	_, err = DB.Exec("INSERT INTO createdAtGolang (username, password) VALUES ('sampleUser', 'samplePassword')")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+*/
